27ed418b8cfcabe917455b14ad0b7a9fccb5905e
MaYiMaMa <2021820970@qq.com>
2026-09-12 15:02:25 +0800

    fix: rename brand from MaYiMaMa (mama) to MaYiMaMa (code)

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const SITE_TITLE = '蚂蚁妈妈';
+export const SITE_TITLE = '蚂蚁码码';
 export const SITE_DESCRIPTION = '记录生活与思考的个人小站';
-export const SITE_AUTHOR = '蚂蚁妈妈';
+export const SITE_AUTHOR = '蚂蚁码码';
